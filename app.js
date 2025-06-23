4c8a0316b0f969ab3a56c09af7b7337a37fed208
kamoliddin <kamoliddin@gmail.com>
2025-06-23 19:05:54 +0500

    a

--- app.js
+++ app.js
@@ -6,43 +6,50 @@ const titl = document.querySelector("body");
 let counter = 0;
 
 pulus.addEventListener("click", () => {
-  if (counter < 15) {
-    counter++;
-    title.textContent = counter;
+  counter++;
+  title.textContent = counter;
+  if (counter == 15) {
+    pulus.disabled = true;
+  } else {
+    pulus.disabled = false;
+  }
+  minus.disabled = false;
 
-    title.classList.remove("green-text", "red-text", "yellow-text");
-    titl.classList.remove("green-bg", "red-bg");
+  title.classList.remove("green-text", "red-text", "yellow-text");
+  titl.classList.remove("green-bg", "red-bg");
 
-    if (counter > 0) {
-      title.classList.add("green-text");
-      titl.classList.add("red-bg");
-    } else if (counter === 0) {
-      title.classList.add("yellow-text");
-      titl.classList.add("green-bg");
-    } else {
-      title.classList.add("red-text");
-      titl.classList.add("green-bg");
-    }
+  if (counter > 0) {
+    title.classList.add("green-text");
+    titl.classList.add("red-bg");
+  } else if (counter === 0) {
+    title.classList.add("yellow-text");
+    titl.classList.add("green-bg");
+  } else {
+    title.classList.add("red-text");
+    titl.classList.add("green-bg");
   }
 });
 
 minus.addEventListener("click", () => {
-  if (counter > -15) {
-    counter--;
-    title.textContent = counter;
+  counter--;
+  title.textContent = counter;
+  if (counter == -15) {
+    minus.disabled = true;
+  } else {
+    minus.disabled = false;
+  }
+  pulus.disabled = false;
+  title.classList.remove("green-text", "red-text", "yellow-text");
+  titl.classList.remove("green-bg", "red-bg");
 
-    title.classList.remove("green-text", "red-text", "yellow-text");
-    titl.classList.remove("green-bg", "red-bg");
-
-    if (counter < 0) {
-      title.classList.add("red-text");
-      titl.classList.add("green-bg");
-    } else if (counter === 0) {
-      title.classList.add("yellow-text");
-      titl.classList.add("red-bg");
-    } else {
-      title.classList.add("green-text");
-      titl.classList.add("red-bg");
-    }
+  if (counter < 0) {
+    title.classList.add("red-text");
+    titl.classList.add("green-bg");
+  } else if (counter === 0) {
+    title.classList.add("yellow-text");
+    titl.classList.add("red-bg");
+  } else {
+    title.classList.add("green-text");
+    titl.classList.add("red-bg");
   }
 });
